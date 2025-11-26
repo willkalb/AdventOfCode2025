6bd43d1518268dbeaa6e670073aa66b4e175ea14
willkalb <62022821+willkalb@users.noreply.github.com>
2025-11-26 00:02:09 -0500

    Update __init__.py
    Update SQL result retrieval to fetchone()

--- SQL/__init__.py
+++ SQL/__init__.py
@@ -4,9 +4,9 @@ def Exec_SQL(day):
     db = Open()
 
     db.execute(f"exec Day{day} @input_as_string = ?, @part = ?", [ "abc,def", 1 ])
-    print("\tPart 1: ", db.fetchone().result)
+    print("\tPart 1: ", db.fetchval())
 
     db.execute(f"exec Day{day} @input_as_string = ?, @part = ?", [ "ghi,jkl", 2 ])
-    print("\tPart 2: ", db.fetchone().result)
+    print("\tPart 2: ", db.fetchval())
 
     Close()
